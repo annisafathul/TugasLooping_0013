cbddcf28e9f3ae0904953d4924300a3388fc4f58
annisafathul <annisafathul18@gmail.com>
2026-03-31 06:53:10 +0700

    membuat function prima

--- Primafibonacci.cpp
+++ Primafibonacci.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int n;
 int pilihan;
+bool isPrima(int x) {
+    if (x <= 1) return false;
+    int i = 2;
+    while (i <= x / 2) {
+        if (x % i == 0) return false;
+        i++;
+    }
+    return true;
+}
